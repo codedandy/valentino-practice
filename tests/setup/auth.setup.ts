0e5a68d42c0523c2d839beaca1f8cff55ef558a2
joe brower <dandy@codedandy.com>
2026-03-18 11:18:47 -0500

    added session storage capability

--- tests/setup/auth.setup.ts
+++ tests/setup/auth.setup.ts
@@ -1,6 +1,8 @@
 import path from 'path';
 import fs from 'fs';
-import test from '@playwright/test';
+import { test, expect } from '../fixtures/fixtures';
+
+const authSesssionFile = path.resolve(__dirname, '../../playwright/.auth/user.json');
 
 // read and parse login.json file
 const loginDataFile = path.resolve(__dirname, '../../playwright/.auth/login.json');
@@ -9,7 +11,16 @@ const loginData = JSON.parse(fs.readFileSync(loginDataFile, 'utf-8')) as {
     valid_password: string;
 };
 
-test('User Login', async ({ page }) => {
-    console.log('User:', loginData.valid_user);
-    console.log('Password:', loginData.valid_password);
-});
+// test('User Login', async ({ page }) => {
+//     console.log('User:', loginData.valid_user);
+//     console.log('Password:', loginData.valid_password);
+// });
+
+test('authenticate', async ({ page, loginPage, topBannerPage }) => {
+    await page.goto('/login');
+    await loginPage.login(process.env.VALID_USER!, process.env.VALID_PASSWORD!);
+    await page.waitForLoadState('networkidle');
+    await expect(topBannerPage.userWelcomeMessage).toBeVisible();
+
+    await page.context().storageState({ path: authSesssionFile });
+})
